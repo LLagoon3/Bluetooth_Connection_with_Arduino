009b5e2842918ce1d786db8cdb112a1d6db42d51
LLagoon3 <choonarm3@gmail.com>
2023-09-16 20:36:39 +0900

    feature: ADD control with switch and timer

--- ConnectWithBluetooth/ConnectWithBluetooth.ino
+++ ConnectWithBluetooth/ConnectWithBluetooth.ino
@@ -2,6 +2,9 @@
 extern volatile unsigned long timer0_millis;
 // SoftwareSerial(RX, TX)
 SoftwareSerial BTSerial(4, 5);
+int led = 13;
+int time_limit = 60;
+bool flag = false;
 
 #include <MemoryFree.h>
 
@@ -10,9 +13,7 @@ void freeMem(){
   Serial.println(freeMemory());
 }
 
-void dataSend(){
-  long randNumber = random(300);
-  String data = String(millis()) + '_' + String(randNumber);
+void dataSend(String data){
 
   byte *temp = new byte[data.length()+1];
   data.getBytes(temp, data.length()+1);
@@ -30,28 +31,55 @@ void dataSend(){
 			BTSerial.write((byte)data[i]);
 		}
   delete[] temp;
-  delay(1000);
+  // delay(1000);
+}
+
+void resetTimer(int time = 0){
+  timer0_millis = time;
+  Serial.println("Reset Timer");////
 }
 
 void setup() {
 	Serial.begin(9600);
   randomSeed(analogRead(0));
 	BTSerial.begin(9600);
+  pinMode(led,OUTPUT);
+}
+
+void chkSwitch(){
+  int i = analogRead(2);
+  Serial.println("\n\nanalogRead : " + String(i));
+  if (i > 800){flag = true;}
+  else{flag = false;}
 }
 
 void loop() {
 
   freeMem();
+  
+  String data;
 
-	if (BTSerial.available()) {
-    while(BTSerial.available() > 0){
-        Serial.println(BTSerial.read());
-    }
-    timer0_millis = 0;
-    Serial.println("Reset Timer");////
-	}
+  // if (BTSerial.available()) {
+  //     while(BTSerial.available() > 0){
+  //         Serial.println(BTSerial.read());
+  //     }
+  //     flag = false;
+  //   }
 
-  dataSend();
+  unsigned long tmpTime = int(millis());
+  if (tmpTime > time_limit * 1000){
+    flag = false;
+    // resetTimer();
+  }
+
+  while(flag == false){
+    chkSwitch();
+    if (flag == true){resetTimer();}
+  }
+  
+  long randNumber = random(300);
+  data = String(millis()) + '_' + String(randNumber);
+  dataSend(data);
 
 }
 
